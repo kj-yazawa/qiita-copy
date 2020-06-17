737d69aec8b2df86abb84611190d95d89b22f781
kj-yazawa <yazawa17ko@gmail.com>
2020-06-17 09:01:06 +0900

    create user article posting form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,11 @@ class ArticlesController < ApplicationController
 
   def create
     @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to articles_url
+    else
+      render :new
+    end
   end
 
   def update
