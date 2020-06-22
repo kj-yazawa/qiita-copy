a9863b88280051dd33d405170147aa6203bf8b65
kj-yazawa <yazawa17ko@gmail.com>
2020-06-22 21:26:51 +0900

    マークダウンで表示できるようredcarpet gemの導入

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,23 +1,27 @@
 module MarkdownHelper
   def markdown(text)
     unless @markdown
-      options = {
-        # no_intra_emphasis: true,
-        # tables: true,
-        # fenced_code_blocks: true,
-        # autolink: true,
-        # disable_indented_code_blocks: true,
-        # strikethrough: true,
-        # lax_spacing: true,
-        # space_after_headers: true,
-        # underline: true,
-        # highlight: true,
-        # hard_wrap: true,
-        # filter_html: true,
-        # xhtml: true,
+      render_options = {
+        filter_html: true,
+        hard_wrap: true,
+        xhtml: true,
+        prettify: true,
+        link_attributes: true,
       }
-      renderer = Redcarpet::Render::HTML.new(options)
-      @markdown = Redcarpet::Markdown.new(renderer)
+      extensions = {
+        no_intra_emphasis: true, # 単語の中の強調を解析しない
+        tables: true, # PHP-Markdown スタイルのテーブルをパースする
+        fenced_code_blocks: true, # PHP-Markdown スタイルのフェンス付きコードブロックをパースする
+        autolink: true, # リンクが <> 文字で囲まれていない場合でもパースする
+        disable_indented_code_blocks: true, # インデント4つで始まるコードブロックをパースしない
+        strikethrough: true, #
+        lax_spacing: true,
+        space_after_headers: true,
+        underline: true,
+        highlight: true,
+      }
+      renderer = Redcarpet::Render::HTML.new(render_options)
+      @markdown = Redcarpet::Markdown.new(renderer, extensions)
     end
 
     @markdown.render(text).html_safe
